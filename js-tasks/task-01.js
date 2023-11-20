584898bc2b78eff7f2c410836c0c1ef0a074aa98
Romamaha <149516586+Romamaha@users.noreply.github.com>
2023-11-20 17:50:27 +0200

    Update task-01.js

--- js-tasks/task-01.js
+++ js-tasks/task-01.js
@@ -6,3 +6,17 @@
 function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
   return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
 }
+
+
+//https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+
+//add your code, choose one better and delete other
+
+
+function makeNegative(num) {
+    if (num > 0) { 
+      return -num;
+  } else{
+    return num;
+  }
+}
