9ea351c8629c15a70583627bdade0f6847f2946c
Olegmagua <olegmag76@gmail.com>
2023-11-21 10:52:43 +0200

    Update task-02.js

--- js-tasks/task-02.js
+++ js-tasks/task-02.js
@@ -4,4 +4,12 @@ function stringToArray(string) {
   return (splitArray = string.split(" "));
 }
 
+//https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
 
+function DNAtoRNA(dna) {
+  dna = dna.split("");
+  for (let i = 0; i < dna.length; i++) {
+    if (dna[i] === "T") dna[i] = "U";
+  }
+  return dna.join("");
+}
