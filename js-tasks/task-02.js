33852dfde9dec60a63f0973832d8f8a25583c0e3
Olegmagua <olegmag76@gmail.com>
2023-11-21 12:24:19 +0200

    Update task-02.js

--- js-tasks/task-02.js
+++ js-tasks/task-02.js
@@ -1,11 +1,15 @@
 //https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
-
+//variant 1
 function stringToArray(string) {
   return (splitArray = string.split(" "));
 }
 
-//https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
+//variant 2
 
+
+
+//https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
+//variant 1
 function DNAtoRNA(dna) {
   dna = dna.split("");
   for (let i = 0; i < dna.length; i++) {
@@ -13,3 +17,17 @@ function DNAtoRNA(dna) {
   }
   return dna.join("");
 }
+
+//variant 2
+
+//https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
+//variant 1
+function min(arr, toReturn) {
+  if (toReturn === "value") {
+    return arr[0];
+  } else if (toReturn === "index") {
+    return arr.indexOf(arr[0]);
+  } else return console.log("Wrong value");
+}
+
+//variant 2
