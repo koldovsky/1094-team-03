ff266e3607fa800aa5511fdd268548bf20ad4458
PetahP <147431814+PetahP@users.noreply.github.com>
2023-11-27 10:54:32 +0000

    rename my js file

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 document.addEventListener('partialsLoaded', () => {
     import('./index.gift-packs.js');
     import('./index.great_products.js');
-    import('./inndex.why-us.js');
+    import('./index.why-us.js');
 
 });
