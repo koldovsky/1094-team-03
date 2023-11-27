7787b435056ca25ca45d1e9abd03b73e71de3046
OleksandraKotovska <oleksandra12om@gmail.com>
2023-11-27 11:39:11 +0200

    updated new arrivals js

--- js/index.new-arrivals.js
+++ js/index.new-arrivals.js
@@ -1,6 +1,36 @@
 const slides = [
-    '<li class="carousel__slide"><img src="img/new-arrivals/old-classic-eather.jfif" alt="The Old Classic Leather Wallet"></li>',
-    '<li class="carousel__slide"><img src="img/new-arrivals/prometheus-case.jfif" alt="The Prometheus Case"></li>' 
+   `<li class="carousel__slide">
+        <a href="#" class="carousel__link">
+            <img src="img/new-arrivals/old-classic-eather.jpg" alt="The Old Classic Leather Wallet">
+        </a>
+        <a href="#" class="carousel__title">The Old Classic Leather Wallet</a>
+        <p class="carousel__price">$43,00</p>
+        <button class="carousel__buy" type="button">Buy</button>
+    </li>`,
+    `<li class="carousel__slide">
+        <a href="#" class="carousel__link">
+            <img src="img/new-arrivals/prometheus-case.jpg" alt="The Prometheus Case">
+        </a>
+        <a href="#" class="carousel__title">The Prometheus Case</a>
+        <p class="carousel__price">$107,00</p>
+        <button class="carousel__buy" type="button">Buy</button>
+    </li>`,
+    `<li class="carousel__slide">
+        <a href="#" class="carousel__link">
+            <img src="img/new-arrivals/norma-leather-tote.jpg" alt="The Norma Leather Tote">
+        </a>
+        <a href="#" class="carousel__title">The Norma Leather Tote</a>
+        <p class="carousel__price">$325,00</p>
+        <button class="carousel__buy" type="button">Buy</button>
+    </li>`,
+    `<li class="carousel__slide">
+        <a href="#" class="carousel__link">
+            <img src="img/new-arrivals/morton-traveler.jpg" alt="The M.T. Morton Traveler">
+        </a>
+        <a href="#" class="carousel__title">The Norma Leather Tote</a>
+        <p class="carousel__price">$370,00</p>
+        <button class="carousel__buy" type="button">Buy</button>
+    </li>`
 ];
 
 let currentSlideIndex = 0;
@@ -11,7 +41,7 @@ function renderSlide() {
     if (window.matchMedia('(min-width:768px)').matches) {
         const secondSlideIndex = currentSlideIndex + 1 >= slides.length ? 0 : currentSlideIndex + 1;
         slideContainer.innerHTML += slides[secondSlideIndex];
-        if (window.matchMedia('(min-width:1024px)').matches) {
+        if (window.matchMedia('(min-width:992px)').matches) {
             const thirdSlideIndex = secondSlideIndex + 1 >= slides.length ? 0 : secondSlideIndex + 1;
             slideContainer.innerHTML += slides[thirdSlideIndex];
         }
@@ -28,13 +58,12 @@ function prevSlide() {
     renderSlide();
 }
 
-// setInterval(nextSlide, 3000);
 renderSlide();
 
-const nextBtn = document.querySelector('.carousel__button right');
+const nextBtn = document.querySelector('.carousel__btn-next');
 nextBtn.addEventListener('click', nextSlide);
 
-const prevBtn = document.querySelector('.carousel__button left');
+const prevBtn = document.querySelector('.carousel__btn-prev');
 prevBtn.addEventListener('click', prevSlide);
 
 window.addEventListener('resize', renderSlide);
