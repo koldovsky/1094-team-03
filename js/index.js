f36acd3f9a5436142c92e80af3eb3894a492521e
unknown <n.krtva@gmail.com>
2023-11-28 10:45:36 +0100

    did link in header to new page Gift packs, added some new files to that page, renamed some files

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 document.addEventListener('partialsLoaded', () => {
-    import('./index.gift-packs.js');
+    import('./index.gift-packs_carousel.js');
     import('./index.great_products.js');
     import('./index.why-us.js');
     import('./index.new-arrivals.js');
