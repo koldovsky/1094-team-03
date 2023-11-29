2880a5e27f51516766f0dcfbef44798d49d64ac5
PetahPavlo <152298900+PetahPavlo@users.noreply.github.com>
2023-11-29 14:36:29 +0000

    making good padding

--- js/index.why-us.js
+++ js/index.why-us.js
@@ -1,20 +1,22 @@
 const slides = [
-    `<div><img src="img/why-us/hand-made.svg" alt="hand-made"><h5 class="whyus__proof">Handmade</h5><p class="whyus__cool">Our craftsmen work with each client individually to create bespoke one-of-a-kind product
-      that reflects their unique taste.</p>
+  `<div><img src="img/why-us/hand-made.svg" alt="hand-made">
+  <h5 class="whyus__proof">Handmade</h5>
+  <p class="whyus__cool">Our craftsmen work with each client individually to create bespoke one-of-a-kind product
+    that reflects their unique taste.</p>
 </div>`,
-    `<div>
+  `<div>
   <img src="img/why-us/top-quality-leather.svg" alt="top-quality-leather">
     <h5 class="whyus__proof">Top-Quality Leather</h5>
     <p class="whyus__cool">We want our work to last for generations, and therefore use only the highest quality
       leather and findings.</p>
 </div>`,
-    `<div>
+  `<div>
   <img src="img/why-us/irish-waxed-linen.svg" alt="irish-waxed-linen">
     <h5 class="whyus__proof">Irish Waxed Linen</h5>
     <p class="whyus__cool">We've sourced the finest quality European linen and other contemporary materials for our
       products.</p>
 </div>`,
-    `<div>
+  `<div>
   <img src="img/why-us/craftsmanship.svg" alt="craftsmanship">
     <h5 class="whyus__proof">Craftsmanship</h5>
     <p class="whyus__cool">We combine traditional techniques and modern manufacturing technologies to create exquisite
@@ -25,26 +27,26 @@ const slides = [
 let currentSlideIndx = 0;
 
 function renderSlide() {
-    const slideContainer = document.querySelector('.whyus__container__inner');
-    slideContainer.innerHTML = slides[currentSlideIndx];
-    if (window.matchMedia('(min-width:768px)').matches) {
-        const secondSlideIndex = currentSlideIndx + 1 >= slides.length ? 0 : currentSlideIndx + 1;
-        slideContainer.innerHTML += slides[secondSlideIndex];
-        if (window.matchMedia('(min-width:992px)').matches) {
-          const thirdSlideIndex = secondSlideIndex + 1 >= slides.length ? 0 : secondSlideIndex + 1;
-          slideContainer.innerHTML += slides[thirdSlideIndex];
-      }
+  const slideContainer = document.querySelector('.whyus__container__inner');
+  slideContainer.innerHTML = slides[currentSlideIndx];
+  if (window.matchMedia('(min-width:768px)').matches) {
+    const secondSlideIndex = currentSlideIndx + 1 >= slides.length ? 0 : currentSlideIndx + 1;
+    slideContainer.innerHTML += slides[secondSlideIndex];
+    if (window.matchMedia('(min-width:992px)').matches) {
+      const thirdSlideIndex = secondSlideIndex + 1 >= slides.length ? 0 : secondSlideIndex + 1;
+      slideContainer.innerHTML += slides[thirdSlideIndex];
     }
+  }
 }
 
 function nextSlide() {
-    currentSlideIndx = currentSlideIndx + 1 >= slides.length ? 0 : currentSlideIndx + 1;
-    renderSlide();
+  currentSlideIndx = currentSlideIndx + 1 >= slides.length ? 0 : currentSlideIndx + 1;
+  renderSlide();
 }
 
 function prevSlide() {
-    currentSlideIndx = currentSlideIndx - 1 < 0 ? slides.length - 1 : currentSlideIndx - 1;
-    renderSlide();
+  currentSlideIndx = currentSlideIndx - 1 < 0 ? slides.length - 1 : currentSlideIndx - 1;
+  renderSlide();
 }
 
 renderSlide();
