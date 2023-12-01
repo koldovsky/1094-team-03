f098504177604feaf941bb6b65190bd7fad4e866
Olegmagua <olegmag76@gmail.com>
2023-12-01 09:53:28 +0000

    subscribe js add

---
+++ js/subscribe_to_get.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const emailInput = document.getElementById('emailInput');
+    emailInput.addEventListener('input', function () {
+      validateEmail(emailInput.value);
+    });
+  });
+
+function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const emailValidation = emailRegex.test(email)
+    if (emailValidation==false){
+        const element=document.querySelector('.subscribe__form__email');
+        element.style.backgroundColor = '--color-for-main-text-on-dark-bg';
+    }
+  }
