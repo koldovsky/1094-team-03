34a92a4c3d58682defe47ff3f21a1d92386c320f
Olegmagua <olegmag76@gmail.com>
2023-12-01 10:27:49 +0000

    subscribe js rework

--- js/subscribe_to_get.js
+++ js/subscribe_to_get.js
@@ -1,38 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
     const emailInput = document.getElementById("emailInput");
-  
     emailInput.addEventListener("input", function () {
       validateEmail(emailInput.value);
-    });
-  
-    emailInput.addEventListener("blur", function () {
-      applyBlurStyles();
     });
   });
   
   function validateEmail(email) {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    const emailValidation = emailRegex.test(email);
-  
-    if (!emailValidation) {
-      applyFocusStyles(true);
-    } else {
-      applyFocusStyles(false);
-    }
-  }
-  
-  function applyFocusStyles(hasError) {
-    const element = document.querySelector(".subscribe__form__email");
-  
-    if (hasError) {
-      element.style.outlineColor = "red";
-    } else {
-      element.style.outlineColor = "";
-    }
-  }
-  
-  function applyBlurStyles() {
-    const element = document.querySelector(".subscribe__form__email");
-    element.style.outlineColor = "";
+    return emailRegex.test(email);
   }
   
