53cbd805e623fcdf9abd2556fd395d2ec62129e6
SEP5155 <serg.papusha@gmail.com>
2023-12-07 00:20:10 +0200

    working on corousel

--- js/notebooks.products.js
+++ js/notebooks.products.js
@@ -6,22 +6,31 @@ const products = document.querySelectorAll('.notebooks-carousel__product');
 console.log(`all products list ${products}`);
 const totalProducts = products.length;
 // console.log(`total amount of products ${totalProducts}`);
-let totalItemsInViewPort = 0;
-// if(window.innerWidth < 767) {
-//     totalItemsInViewPort = 1;
-//     console.log(`total amount of products ${totalProducts}`);
-
-// } else if(window.innerWidth > 768 && window.innerWidth < 991) {
-//     totalItemsInViewPort = 2;
-//     console.log(`total amount of products ${totalProducts}`);
-// } else {
-//     totalItemsInViewPort = 3;
-//     console.log(`total amount of products ${totalProducts}`);
-// }
+let totalItemsInViewPort = calculatedItemsInViewPort();
 
 document.getElementById('next-button').addEventListener('click', showNext);
 document.getElementById('prev-button').addEventListener('click', showPrev);
 
+window.addEventListener('resize', function() {
+    totalItemsInViewPort = calculatedItemsInViewPort();
+    updateCarousel();
+})
+
+function calculatedItemsInViewPort() {
+    if(window.innerWidth < 767) {
+        totalItemsInViewPort = 1;
+        console.log(`total amount of products ${totalProducts}`);
+        return 1;
+    } else if(window.innerWidth > 768 && window.innerWidth < 991) {
+        totalItemsInViewPort = 2;
+        console.log(`total amount of products ${totalProducts}`);
+        return 2
+    } else {
+        totalItemsInViewPort = 3;
+        console.log(`total amount of products ${totalProducts}`);
+        return 3;
+    }
+}
 function showNext() {
     products[currentIndex].classList.remove('active');
     currentIndex = (currentIndex + 1) % totalProducts;
@@ -37,17 +46,6 @@ function showPrev() {
 function updateCarousel() {
     for (let i = 0; i < totalProducts; i++) {
         products[i].classList.remove('active');
-    }
-    if(window.innerWidth < 767) {
-        totalItemsInViewPort = 1;
-        console.log(`total amount of products ${totalProducts}`);
-    
-    } else if(window.innerWidth > 768 && window.innerWidth < 991) {
-        totalItemsInViewPort = 2;
-        console.log(`total amount of products ${totalProducts}`);
-    } else {
-        totalItemsInViewPort = 3;
-        console.log(`total amount of products ${totalProducts}`);
     }
 
     let startIdx = currentIndex;
