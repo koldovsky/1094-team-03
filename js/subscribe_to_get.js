c631af9a19eb327c6d195c2aeb43322f66aea471
Olegmagua <olegmag76@gmail.com>
2023-12-06 19:08:42 +0000

    rework subsribe

--- js/subscribe_to_get.js
+++ js/subscribe_to_get.js
@@ -1,12 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const emailInput = document.getElementById("emailInput");
-    emailInput.addEventListener("input", function () {
-      validateEmail(emailInput.value);
-    });
+  const emailInput = document.getElementById("emailInput");
+  emailInput.addEventListener("input", function () {
+    validateEmail(emailInput.value);
   });
-  
-  function validateEmail(email) {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
+});
+var emailList = [];
+function validateEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (emailRegex.test(email)===true) {
+    event.preventDefault();
+    var emailInput = document.getElementById("emailInput");
+    var emailValue = emailInput.value;
+    emailList.push(emailValue);
+    console.log("Thanks for subscribe Handmade Shop dear" + emailList);
   }
-  
+  return emailRegex.test(email);
+}
