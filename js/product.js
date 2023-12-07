2e8ea46198cac6a07b32065e49addd07480f5a78
Romamaha <149516586+Romamaha@users.noreply.github.com>
2023-12-07 10:55:17 +0000

    update new element is me code

--- js/product.js
+++ js/product.js
@@ -1,21 +1,30 @@
 const endDate = new Date(2023, 11, 8, 23, 59, 59);
 
-  function updateCountdown() {
+
+
+function updateCountdown() {
     const now = new Date();
     const timeDifference = endDate - now;
+    const day = document.querySelector('#days')
+    const hour = document.querySelector('#hours')
+    const minute = document.querySelector('#minutes')
+    const second = document.querySelector('#seconds')
 
     if (timeDifference > 0) {
-      const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-      const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-      const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+        const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
 
-      document.getElementById('countdown').innerHTML = `${days}д ${hours}г ${minutes}хв ${seconds}с`;
+        day.textContent = days + 'д'
+        hour.textContent = hours + 'г'
+        minute.textContent = minutes + 'хв'
+        second.textContent = seconds + 'с'
     } else {
-      document.getElementById('countdown').innerHTML = 'Акція завершилась!';
+        document.getElementById('countdown').innerHTML = 'Акція завершилась!';
     }
-  }
+}
 
-  setInterval(updateCountdown, 1000);
+setInterval(updateCountdown, 1000);
 
-  updateCountdown();
+updateCountdown();
